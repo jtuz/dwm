fded468bfd8d7bd7d734f55ff502ac881db69548
Jacob Tuz Poot <jetp79@gmail.com>
2021-03-06 18:03:28 -0600

    Add color bar

--- config.def.h
+++ config.def.h
@@ -18,12 +18,18 @@ static char normbordercolor[]       = "#444444";
 static char selbgcolor[]            = "#005577";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#79740E";
-static const unsigned int baralpha = 0xd0;
+static const unsigned int baralpha = 0x00;
+// static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-    [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-    [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+    [SchemeNorm]     = { normfgcolor, normbgcolor, normbordercolor },
+    [SchemeSel]      = { selfgcolor,  selbgcolor,  selbordercolor  },
+	[SchemeStatus]   = { "#EBDBB2", "#D65D0E",  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { "#EBDBB2", "#98971A",  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+    [SchemeTagsNorm] = { "#A89984", "#282828",  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]  = { "#EBDBB2", "#3C3836",  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm] = { "#A89984", "#282828",  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
@@ -32,7 +38,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
